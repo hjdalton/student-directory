0ffdf09ecb2e5ed20eda85c935df2be133eafec8
hjdalton <hjdalton@hotmail.co.uk>
2021-07-16 12:33:59 +0100

    Future refractioning of interactive menu method

--- directory.rb
+++ directory.rb
@@ -1,28 +1,46 @@
 #the list of students in an array
-students = [
-  {name: "Harry Potter", cohort: :november},
-  {name: "Ron Weasley", cohort: :november},
-  {name: "Hermione Grainger", cohort: :november},
-  {name: "Draco Malfoy", cohort: :november},
-  {name: "Luna Lovegood", cohort: :november},
-  {name: "Fred Weasley", cohort: :november},
-  {name: "George Weasley", cohort: :november},
-  {name: "Ginny Weasley", cohort: :november},
-  {name: "Colin Creevey", cohort: :november},
-  {name: "Choo Chang", cohort: :november},
-  {name: "Cedric Diggory", cohort: :november},
-]
+@students = []
+
 def input_students
   puts "Enter the name of the new student"
   puts "To finish, hit return twice"
-  students = []
   name = gets.chomp
   while !name.empty? do 
-    students << {name: name, cohort: :november}
-    puts "We have registered #{students.count} students"
+    @students << {name: name, cohort: :november}
+    puts "We have registered #{@students.count} students"
     name = gets.chomp
   end
-  students
+end
+
+def interactive_menu
+  loop do 
+    print_menu
+    process(gets.chomp)
+  end
+end
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
+
+def show_students
+  print_header
+  print_student_list
+  print_footer
+end
+
+def process(selection)
+  case selection 
+  when "1" 
+    input_students
+  when "2"
+   show_students
+  when "9"
+    exit
+  else puts "Please try again"
+  end
 end
 
 def print_header
@@ -30,35 +48,14 @@ def print_header
   puts "-------------"
 end
 
-def print(names)
-  names.each do |name|
-  puts "#{name[:name]} (#{name[:cohort]} cohort)"
+def print_student_list
+  @students.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
-end 
-
-def print_footer(names)
-  puts "Overall, we have #{names.count} great students"
 end
 
-def interactive_menu
-  students = []
-  loop do 
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit"
-    selection = gets.chomp
-    case selection 
-    when "1" 
-      students = input_students
-    when "2"
-      print_header
-      print(students)
-      print_footer(students)
-    when "9"
-      exit
-    else puts "Please try again"
-    end
-  end
+def print_footer
+  puts "Overall, we have #{@students.count} great students"
 end
 
 interactive_menu()
