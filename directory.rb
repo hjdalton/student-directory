2f98c4f3f1d1ba2436affee042364bff44494c46
hjdalton <hjdalton@hotmail.co.uk>
2021-07-16 11:48:47 +0100

    converting to hash data structure from an array

--- directory.rb
+++ directory.rb
@@ -1,26 +1,26 @@
 #the list of students in an array
 students = [
-"Dr. Hannibal Lecter", 
-"Darth Vader", 
-"Nurse Ratched", 
-"Michael Corleone", 
-"Alex DeLarge", 
-"The Wicked Witch of the West", 
-"Terminator", 
-"Freddy Krueger", 
-"The Joker", 
-"Joffrey Baratheon", 
-"Norman Bates"
+  {name: "Harry Potter", cohort: :november},
+  {name: "Ron Weasley", cohort: :november},
+  {name: "Hermione Grainger", cohort: :november},
+  {name: "Draco Malfoy", cohort: :november},
+  {name: "Luna Lovegood", cohort: :november},
+  {name: "Fred Weasley", cohort: :november},
+  {name: "George Weasley", cohort: :november},
+  {name: "Ginny Weasley", cohort: :november},
+  {name: "Colin Creevey", cohort: :november},
+  {name: "Choo Chang", cohort: :november},
+  {name: "Cedric Diggory", cohort: :november},
 ]
 
 def print_header
-  puts "The students of Villains Academy"
+  puts "The students of Hogwarts School for witchcraft and wizardry"
   puts "-------------"
 end
 
 def print(names)
   names.each do |name|
-  puts name
+  puts "#{name[:name]} (#{name[:cohort]} cohort)"
   end
 end 
 
